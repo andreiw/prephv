5d2543a65a9d1468e99e2fbbf05d3141fb1cdb80
Andrei Warkentin <andrey.warkentin@gmail.com>
2015-05-12 09:03:05 -0400

    ppc64le_hello: ready for github
    Yup...
    
    Signed-off-by: Andrei Warkentin <andrey.warkentin@gmail.com>

--- main.c
+++ main.c
@@ -9,12 +9,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *           
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *                
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -27,11 +27,19 @@
 #define HELLO_OPAL "Hello OPAL!\n"
 #define OPAL_TERMINAL_0 0
 
+
 void
 c_main(u64 dt_base)
 {
 	u64 len = cpu_to_be64(sizeof(HELLO_OPAL));
 
+	/*
+	 * Write using sim interface (simpler).
+	 */
 	mambo_write(HELLO_MAMBO, sizeof(HELLO_MAMBO));
+
+	/*
+	 * Write using firmware interface.
+	 */
 	opal_write(OPAL_TERMINAL_0, &len, HELLO_OPAL);
 }
