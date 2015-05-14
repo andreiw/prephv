00b722d03b584956115240cf659a7769d9d74768
Andrei Warkentin <andrey.warkentin@gmail.com>
2015-05-14 03:55:31 -0400

    powerpc64le_hello: fixup README and libfdt_env.h
    Be clearer that FDT is always BE.
    
    Signed-off-by: Andrei Warkentin <andrey.warkentin@gmail.com>

--- libfdt_env.h
+++ libfdt_env.h
@@ -5,20 +5,12 @@
 #include <types.h>
 #include <string.h>
 #include <stdarg.h>
+#include <endian.h>
 
-#define _B(n)	((unsigned long long)((uint8_t *)&x)[n])
-static inline uint32_t fdt32_to_cpu(uint32_t x)
-{
-	return (_B(0) << 24) | (_B(1) << 16) | (_B(2) << 8) | _B(3);
-}
+#define fdt32_to_cpu(x) be32_to_cpu(x)
 #define cpu_to_fdt32(x) fdt32_to_cpu(x)
 
-static inline uint64_t fdt64_to_cpu(uint64_t x)
-{
-	return (_B(0) << 56) | (_B(1) << 48) | (_B(2) << 40) | (_B(3) << 32)
-		| (_B(4) << 24) | (_B(5) << 16) | (_B(6) << 8) | _B(7);
-}
+#define fdt64_to_cpu(x) be64_to_cpu(x)
 #define cpu_to_fdt64(x) fdt64_to_cpu(x)
-#undef _B
 
 #endif /* _LIBFDT_ENV_H */
